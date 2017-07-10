fd262fd54adc92dace55ee5603577c605340a693
Brok Bucholtz <brok.bucholtz@gmail.com>
2017-07-10 14:53:35 -0700

    Add optional task to apply inference on video

--- main.py
+++ main.py
@@ -115,6 +115,8 @@ def run():
         # TODO: Save inference data using helper.save_inference_samples
         #  helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
 
+        # OPTIONAL: Apply the trained model to a video
+
 
 if __name__ == '__main__':
     run()
