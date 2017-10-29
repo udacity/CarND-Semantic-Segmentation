01f15b8e35a14adea1293a9069d3633161d98737
robroooh <robroo.pc@gmail.com>
2017-10-29 21:29:46 +0100

    use named arguments for softmax cross entropy

--- main.py
+++ main.py
@@ -103,7 +103,7 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :return: Tuple of (logits, train_op, cross_entropy_loss)
     """
     logits = tf.reshape(nn_last_layer, (-1, num_classes))
-    cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits, correct_label))
+    cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=correct_label))
     train_op = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy_loss)
 
     return logits, cross_entropy_loss, train_op
