5444bb9c2d33a6a163b1052c9ad3718805eebe6b
robroooh <robroo.pc@gmail.com>
2017-10-29 21:23:14 +0100

    fix model load's tag

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def load_vgg(sess, vgg_path):
     :return: Tuple of Tensors from VGG model (image_input, keep_prob, layer3_out, layer4_out, layer7_out)
     """
     #   Use tf.saved_model.loader.load to load the model and weights
-
+   
     vgg_tag = 'vgg16'
     vgg_input_tensor_name = 'image_input:0'
     vgg_keep_prob_tensor_name = 'keep_prob:0'
@@ -33,7 +33,7 @@ def load_vgg(sess, vgg_path):
     vgg_layer4_out_tensor_name = 'layer4_out:0'
     vgg_layer7_out_tensor_name = 'layer7_out:0'
 
-    graph = tf.saved_model.loader.load(sess, vgg_tag, vgg_path)
+    graph = tf.saved_model.loader.load(sess, [vgg_tag], vgg_path)
     image_input = graph.get_tensor_by_name(vgg_input_tensor_name)
     keepprob_out = graph.get_tensor_by_name(vgg_keep_prob_tensor_name)
     layer3_out = graph.get_tensor_by_name(vgg_layer3_out_tensor_name)
