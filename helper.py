cae4c0e982ca48d11b2f3ce4214b19249832fd96
robroooh <robroo.pc@gmail.com>
2018-05-22 23:43:06 +0200

    make it class 0,1,2

--- helper.py
+++ helper.py
@@ -62,12 +62,14 @@ def maybe_download_pretrained_vgg(data_dir):
 
 
 def preprocess_labels(label_image):
-    labels_new = np.copy(label_image)
-    # Identify lane marking pixels (label is 6)
+    labels_new = np.zeros_like(label_image)
     lane_marking_pixels = (label_image[:,:,0] == 6).nonzero()
-    # Set lane marking pixels to road (label is 7)
-    labels_new[lane_marking_pixels] = 7
-    # Identify all vehicle pixels
+    labels_new[lane_marking_pixels] = 1
+    lane_marking_pixels = (label_image[:,:,0] == 7).nonzero()
+    labels_new[lane_marking_pixels] = 1
+    lane_marking_pixels = (label_image[:,:,0] == 10).nonzero()
+    labels_new[lane_marking_pixels] = 2
+
     vehicle_pixels = (label_image[:,:,0] == 10).nonzero()
     # Isolate vehicle pixels associated with the hood (y-position > 496)
     hood_indices = (vehicle_pixels[0] >= 496).nonzero()[0]
@@ -75,9 +77,7 @@ def preprocess_labels(label_image):
                    vehicle_pixels[1][hood_indices])
     # Set hood pixel labels to 0
     labels_new[hood_pixels] = 0
-    for i in [1,2,3,4,5,8,9,11,12]:
-        otherclass_pixels = (label_image[:,:,0] == i).nonzero()
-        labels_new[otherclass_pixels] = 0
+    # Identify lane marking pixels (label is 6)
     return labels_new
 
 def process_carla(BASE_DIR='Train'):
@@ -125,6 +125,74 @@ def split_data(BASE_DIR='data_road'):
                  os.path.join(BASE_DIR,"valid", "image_2", os.path.basename(x)))
         os.rename(os.path.join(BASE_DIR,'full_training', 'gt_image_2',os.path.basename(y)),
              os.path.join(BASE_DIR,"valid", "gt_image_2", os.path.basename(y)))
+
+def gen_batch_carla_function(data_folder, image_shape, train=True):
+    """
+    Generate function to create batches of training data
+    :param data_folder: Path to folder that contains all the datasets
+    :param image_shape: Tuple - Shape of image
+    :return:
+    """
+    _train = train
+    seq = iaa.Sequential([
+        iaa.Fliplr(0.5), # horizontal flips
+        iaa.Crop(percent=(0, 0.1)), # random crops
+        # Small gaussian blur with random sigma between 0 and 0.5.
+        # But we only blur about 50% of all images.
+        # iaa.Sometimes(0.5,
+        #     iaa.GaussianBlur(sigma=(0, 0.5))
+        # ),
+        # Strengthen or weaken the contrast in each image.
+        iaa.ContrastNormalization((0.75, 1.5)),
+        # Add gaussian noise.
+        # For 50% of all images, we sample the noise once per pixel.
+        # For the other 50% of all images, we sample the noise per pixel AND
+        # channel. This can change the color (not only brightness) of the
+        # pixels.
+        # iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05*255), per_channel=0.5),
+        # Make some images brighter and some darker.
+        # In 20% of all cases, we sample the multiplier once per channel,
+        # which can end up changing the color of the images.
+        # iaa.Multiply((0.8, 1.2), per_channel=0.2),
+        # Apply affine transformations to each image.
+        # Scale/zoom them, translate/move them, rotate them and shear them.
+        iaa.Affine(
+            scale={"x": (0.8, 1.2), "y": (0.8, 1.2)},
+            translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)},
+            rotate=(-15, 15)
+           # shear=(-8, 8)
+        )
+    ], random_order=True) # apply augmenters in random order
+
+    def get_batches_fn(batch_size):
+        """
+        Create batches of training data
+        :param batch_size: Batch Size
+        :return: Batches of training data
+        """
+        image_paths = glob(os.path.join(data_folder, 'CameraRGB', '*.png'))
+        label_paths = {os.path.basename(path): path for path in glob(os.path.join(data_folder, 'gt_image_2', '*_road_*.png'))}
+        background_color = np.array([255, 0, 0])
+
+        random.shuffle(image_paths)
+        for batch_i in range(0, len(image_paths), batch_size):
+            images = []
+            gt_images = []
+            for image_file in image_paths[batch_i:batch_i+batch_size]:
+                gt_image_file = label_paths[os.path.basename(image_file)]
+                image = scipy.misc.imresize(scipy.misc.imread(image_file), image_shape)
+                gt_image = scipy.misc.imresize(scipy.misc.imread(gt_image_file), image_shape)
+                if _train:
+                    seq_det = seq.to_deterministic()
+                    image = seq_det.augment_image(image)
+                    gt_image = seq_det.augment_image(gt_image)
+                gt_bg = np.all(gt_image == background_color, axis=2)
+                gt_bg = gt_bg.reshape(*gt_bg.shape, 1)
+                gt_image = np.concatenate((gt_bg, np.invert(gt_bg)), axis=2)
+                images.append(image)
+                gt_images.append(gt_image)
+            yield np.array(images), np.array(gt_images)
+    return get_batches_fn
 
 def gen_batch_function(data_folder, image_shape, train=True):
     """
@@ -195,7 +263,6 @@ def gen_batch_function(data_folder, image_shape, train=True):
                 gt_images.append(gt_image)
             yield np.array(images), np.array(gt_images)
     return get_batches_fn
-
 
 def gen_test_output(sess, logits, keep_prob, image_pl, data_folder, image_shape):
     """
